31e318cfd64d05c815ebcefd2b7717f97e7aa84f
dustinpfister <dustin.pfister@gmail.com>
2017-12-23 10:33:22 -0500

    index.js is now cleaner

--- workshops/blog/index.js
+++ workshops/blog/index.js
@@ -1,64 +1,15 @@
 
-var yaml = require('js-yaml'),
-marked = require('marked'),
-dir = require('node-dir'),
-mkdirp = require('mkdirp'),
-ejs = require('ejs'),
-path = require('path'),
-fs = require('fs'),
-_ = require('lodash'),
 
+let html = require('./lib/html.js'),
 
-html = require('./lib/html.js'),
-
-pat_md = /.md$/,
-pat_header = /---[\s|\S]*---/,
-pat_dash = /---/g;
-
-/*
-var buildPosts = function (conf, db, done) {
-
-    done = done || function () {};
-    console.log('ready to build posts.');
-
-    var i = 0,
-    len = db.reports.length;
-
-    loop = function () {
-
-        require('./lib/html.js').post(conf, db.reports[i], function () {
-
-            i += 1;
-
-            if (i < len) {
-
-                loop();
-
-            } else {
-
-                console.log('all done');
-                done();
-
-            }
-
-        })
-
-    };
-
-    loop();
-
-};
-*/
-
-var build = function (conf) {
+build = function (conf) {
 
     // build the database
     require('./lib/build_db.js').build(conf, function (db) {
 
         // build posts
-        //buildPosts(conf, db, function () {
-html.years(conf,db,function(){
-		
+        html.years(conf, db, function () {
+
             console.log('ready to build pages');
 
         });
@@ -88,7 +39,6 @@ if (require.main === module) {
 
     // being used as a module with require
 
-    //exports.build = build;
     exports.build = build;
 
 }
