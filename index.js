ee767edc83fc348732ce1d16b3b9a49b0d62dfb0
dustinpfister <dustin.pfister@gmail.com>
2017-12-25 21:09:53 -0500

    new build proceess working great

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+let _ = require('lodash');
+
 let conf = {
 
     source: './source/_posts',
@@ -24,24 +26,50 @@ let conf = {
 /*
 require('./workshops/blog/index.js').build(conf, function () {
 
-    conf.layout = './workshops/indexer/theme/layout.ejs';
+conf.layout = './workshops/indexer/theme/layout.ejs';
 
-    require('./workshops/indexer/index.js').build(conf, function () {
+require('./workshops/indexer/index.js').build(conf, function () {
 
-        console.log('done with index');
-
-    });
+console.log('done with index');
 
 });
-*/
 
-require('./lib/crawl.js').crawl().then(function (files) {
+});
+ */
 
-console.log(files);
+require('./lib/crawl.js').crawl().then(function (report) {
+
+    let i = 0,
+    len = report.ws.length,
+    ws = report.ws[i];
+
+    // what to do when done with a workshop
+    onDone = function () {
+
+        console.log('done with: ' + ws.name);
+
+        i += 1;
+        if (i < len) {
+            ws = report.ws[i];
+            next();
+
+        }
+
+    },
+
+    // what to do for the next workshop
+    next = function () {
+
+        conf.layout = ws.theme.layout;
+
+        require('./workshops/' + report.ws[i].name + '/index.js').build(conf, onDone);
+
+    };
+
+    next();
 
 }).catch (function (err) {
 
-console.log(err);
+    console.log(err);
 
 });
-
