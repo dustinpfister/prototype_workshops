2d82e448bde0192dc9a1a44966d7c0f35fc29848
dustinpfister <dustin.pfister@gmail.com>
2017-12-26 17:17:46 -0500

    new build reports

--- workshops/lexter/lib/build.js
+++ workshops/lexter/lib/build.js
@@ -2,12 +2,44 @@
 
 build.js for lexter
 
-*/
-
+ */
 
 let cheerio = require('cheerio'),
 path = require('path'),
 dir = require('node-dir');
+
+// read the whole target path in conf.target, and runt the forFile method for each file found
+let readTarget = function (conf, forFile) {
+
+    return new Promise(function (resolve, reject) {
+
+        dir.readFiles(conf.target, {
+
+            match: /.html$/
+
+        }, function (err, content, filename, next) {
+
+            if (err) {
+
+                reject(err);
+
+            }
+
+            forFile(err, content, filename, function () {
+
+                next();
+
+            });
+
+        }, function () {
+
+            resolve();
+
+        });
+
+    });
+
+};
 
 // old buildReports method
 exports.buildReports = function (conf, done) {
@@ -16,14 +48,10 @@ exports.buildReports = function (conf, done) {
 
     let reports = [];
 
-    //process.chdir(conf.target);
+    // using my read targets method
+    readTarget(conf, function (err, content, filename, next) {
 
-    dir.readFiles(conf.target, {
-
-        match: /.html$/
-
-    }, function (err, content, filename, next) {
-
+        // for each file
         let $ = cheerio.load(content);
 
         // paragraphs?
@@ -40,13 +68,21 @@ exports.buildReports = function (conf, done) {
 
         next();
 
-    }, function () {
+    }).then(function () {
 
+        // when done
         reports.sort(function (a, b) {
 
             return b.wordCount - a.wordCount;
 
         });
+
+        done(reports);
+
+    }).catch (function (err) {
+
+        // if error
+        console.log(err);
 
         done(reports);
 
