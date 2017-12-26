fe1bb356a897cebf90959c51e37b991715461c7c
dustinpfister <dustin.pfister@gmail.com>
2017-12-26 00:37:50 -0500

    looking at html

--- workshops/lexter/index.js
+++ workshops/lexter/index.js
@@ -1,7 +1,24 @@
 
+let dir = require('node-dir');
+
 exports.build = function (conf, done) {
 
-   console.log('lexter');
-   done();
+    console.log('lexter');
+
+    dir.readFiles(conf.target, {
+
+        match: /.html$/
+
+    }, function (err, content, filename, next) {
+
+        console.log('lexter: ' + filename);
+
+        next();
+
+    }, function () {
+
+        done();
+
+    });
 
 };
