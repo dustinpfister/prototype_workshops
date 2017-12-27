348a84e46722b72a12f2826878e67474c5ff1dd4
dustinpfister <dustin.pfister@gmail.com>
2017-12-27 14:56:15 -0500

    using lowdb in build process for lexter

--- workshops/lexter/lib/build.js
+++ workshops/lexter/lib/build.js
@@ -7,7 +7,18 @@ build.js for lexter
 let cheerio = require('cheerio'),
 path = require('path'),
 natural = require('natural'),
-dir = require('node-dir');
+dir = require('node-dir'),
+
+// low db
+low = require('lowdb'),
+FileSync = require('lowdb/adapters/FileSync'),
+adapter = new FileSync('./db_lexter.json'),
+db = low(adapter);
+
+db.defaults({
+    reports: [],
+    keywords: []
+}).write();
 
 /* find if a test path is a sub path of another root path
 https://stackoverflow.com/questions/37521893/determine-if-a-path-is-subdirectory-of-another-in-node-js
@@ -69,6 +80,29 @@ let tokenize = function (text) {
 
 };
 
+let db_rec = function (report) {
+
+    let rec = db.get('reports').find({
+            filename: report.filename
+        }).value();
+
+    if (!rec) {
+
+        db.get('reports').push(report).write();
+
+    } else {
+
+        db.get('reports')
+        .find({
+            filename: report.filename
+        })
+        .assign(report)
+        .write()
+
+    }
+
+}
+
 // buildReports method
 exports.buildReports = function (conf, done) {
 
@@ -87,12 +121,16 @@ exports.buildReports = function (conf, done) {
 
             let report = {};
 
+            report.filename = filename;
             report.href = filename.replace(path.basename(conf.target), '');
             report.text = $('body').text();
             report.tokens = tokenize(report.text.toLowerCase());
             report.wordCount = report.tokens.length;
 
             reports.push(report);
+
+            // push or update a database record using lowdb
+            db_rec(report);
 
         }
 
