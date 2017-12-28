1209a0dca1e7a73c9ff24224cd02da4d4245c4ea
dustinpfister <dustin.pfister@gmail.com>
2017-12-27 19:05:30 -0500

    getting collection folders in source

--- workshops/jimpster/index.js
+++ workshops/jimpster/index.js
@@ -1,7 +1,24 @@
+let fs = require('fs');
+
 exports.build = function (conf, done) {
+
+    let self = this;
 
     this.log('jimpster');
 
-    done();
+    fs.readdir(conf.source, function (err, collections) {
+
+        if (err) {
+
+            self.log('error getting collections source. Check source/img/_jimpster path');
+
+            done();
+
+        }
+
+        self.log(collections);
+
+        done();
+    });
 
 };
