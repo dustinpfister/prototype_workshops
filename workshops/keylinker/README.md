e0fe04dcb40847a592dfa7cbeeb3239e0c7834d3
dustinpfister <dustin.pfister@gmail.com>
2017-12-30 12:02:04 -0500

    added keylinker workshop

---
+++ workshops/keylinker/README.md
@@ -0,0 +1,3 @@
+# Keylinker workshop
+
+
---
+++ workshops/keylinker/index.js
@@ -0,0 +1,16 @@
+
+exports.db = function (conf, done) {
+
+    this.log('I am keylinkers database method:', 'error');
+
+    done();
+
+};
+
+exports.build = function (conf, done) {
+
+    this.log('I am keylinkers build method', 'error');
+
+    done();
+
+};
